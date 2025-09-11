28ac3e372f02d78c91809a97cd2ce283cd7af801
Moritz344 <moritzmaier34@gmail.com>
2025-09-11 17:22:51 +0200

    header component/leftbar component

---
+++ book-page/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
+import { LeftBarComponent } from '../left-bar/left-bar.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,HeaderComponent,LeftBarComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'book-page';
+}
+
+
